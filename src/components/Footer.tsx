153ae9d6ed9e851c04cdf7a8e08aa9fee0e22425
João Medeiros <jpmdrsdev@gmail.com>
2025-04-15 16:54:52 -0300

    feat: creating footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className="py-8 bg-primary text-white text-center">
+      <p>
+        © {new Date().getFullYear()} João Pedro Medeiros. Todos os direitos
+        reservados.
+      </p>
+    </footer>
+  )
+}
