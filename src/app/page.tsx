691ec61820f83ce02e43bcbe32fffdba7ef23295
João Medeiros <jpmdrsdev@gmail.com>
2025-04-15 17:26:27 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Footer } from "@/components/Footer"
 import Navbar from "@/components/Navbar"
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Footer />
     </main>
   )
 }
