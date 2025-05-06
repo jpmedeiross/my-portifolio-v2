25b6d5d071e7795bf99b024929abaebd18997835
João Medeiros <jpmdrsdev@gmail.com>
2025-05-06 15:44:26 -0300

    chore: navbar adjusts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import { ModeToggle } from "@/components/ModeToggle"
+import { Link } from "lucide-react"
+import { Button } from "./ui/button"
 
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4 bg-background">
-      <div className="text-lg font-bold">João Pedro</div>
+      <div className="text-lg font-bold">João.</div>
       <div className="flex items-center space-x-4">
         <a href="#home" className="hover:underline">
           Home
+        </a>
+        <a href="#about" className="hover:underline">
+          Sobre mim
         </a>
         <a href="#projects" className="hover:underline">
           Projetos
@@ -14,6 +19,12 @@ export default function Navbar() {
         <a href="#contact" className="hover:underline">
           Contato
         </a>
+        <Button
+          size={"icon"}
+          className="rounded-full bg-background text-balance"
+        >
+          <Link href="https://github.com/jpmedeiross">Teste</Link>
+        </Button>
         <ModeToggle />
       </div>
     </nav>
