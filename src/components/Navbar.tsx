6a7f472f6c9d5fbe3f63c1e9da0e63b0ccc74953
João Medeiros <jpmdrsdev@gmail.com>
2025-05-06 16:23:48 -0300

    feat: adding animation for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,33 @@
 "use client"
 
+import { motion } from "framer-motion"
 import Link from "next/link"
 import { ModeToggle } from "./ModeToggle"
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-white dark:bg-black shadow-md p-4 flex justify-between items-center">
-      <span className="text-2xl font-bold">&lt; João /&gt;</span>
+    <motion.nav
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className="sticky top-0 z-50 bg-white dark:bg-black shadow-md p-4 flex justify-between items-center"
+    >
+      <h1 className="text-2xl font-bold">&lt; João /&gt;</h1>
       <div className="flex gap-6 text-lg">
         <Link href="#sobre" scroll={false}>
           Sobre
         </Link>
-        <Link href="#projetos" scroll={false}>
-          Projetos
-        </Link>
         <Link href="#skills" scroll={false}>
           Habilidades
+        </Link>
+        <Link href="#projetos" scroll={false}>
+          Projetos
         </Link>
         <Link href="#contato" scroll={false}>
           Contato
         </Link>
         <ModeToggle />
       </div>
-    </nav>
+    </motion.nav>
   )
 }
