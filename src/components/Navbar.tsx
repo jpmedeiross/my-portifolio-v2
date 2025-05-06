52d3554dbf5a3b0ad94cf5b54079651b74eb207d
João Medeiros <jpmdrsdev@gmail.com>
2025-05-06 17:22:00 -0300

    feat: hoover in navbar elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,16 +14,48 @@ export default function Navbar() {
     >
       <h1 className="text-4xl font-semibold font-roboto">&lt; João /&gt;</h1>
       <div className="flex gap-6 text-lg">
-        <Link href="#sobre" scroll={false} className="font-inter">
+        <Link
+          href="#sobre"
+          scroll={false}
+          className="font-inter relative inline-block font-inter text-black dark:text-white
+             after:content-[''] after:absolute after:left-0 after:bottom-0 
+             after:h-[2px] after:w-0 after:bg-blue-500 
+             after:transition-all after:duration-300 
+             hover:after:w-full"
+        >
           Sobre
         </Link>
-        <Link href="#skills" scroll={false} className="font-inter">
+        <Link
+          href="#skills"
+          scroll={false}
+          className="relative inline-block font-inter text-black dark:text-white
+             after:content-[''] after:absolute after:left-0 after:bottom-0 
+             after:h-[2px] after:w-0 after:bg-blue-500 
+             after:transition-all after:duration-300 
+             hover:after:w-full"
+        >
           Habilidades
         </Link>
-        <Link href="#projetos" scroll={false} className="font-inter">
+        <Link
+          href="#projetos"
+          scroll={false}
+          className="relative inline-block font-inter text-black dark:text-white
+             after:content-[''] after:absolute after:left-0 after:bottom-0 
+             after:h-[2px] after:w-0 after:bg-blue-500 
+             after:transition-all after:duration-300 
+             hover:after:w-full"
+        >
           Projetos
         </Link>
-        <Link href="#contato" scroll={false} className="font-inter">
+        <Link
+          href="#contato"
+          scroll={false}
+          className="relative inline-block font-inter text-black dark:text-white
+             after:content-[''] after:absolute after:left-0 after:bottom-0 
+             after:h-[2px] after:w-0 after:bg-blue-500 
+             after:transition-all after:duration-300 
+             hover:after:w-full"
+        >
           Contato
         </Link>
         <ModeToggle />
